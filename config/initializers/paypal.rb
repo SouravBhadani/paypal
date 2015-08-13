4abfd9468ff30826ee6f6b524ce388f93ffdea3b
Sourav Kumar <souravbhadani94@gmail.com>
2015-08-13 12:16:03 +0530

    Update paypal.rb

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,6 +1,6 @@
 PayPal::Recurring.configure do |config|
   config.sandbox = true
-  config.username = "souravbhadani94-facilitator_api1.gmail.com"
-  config.password = "2Q8QHCMMZFXC986R"
-  config.signature = "ARBGJ6rCmC4oPAM4HQAjoMsIgp0VA-L08oV8Bgw8tABtiNm92OOHqfZh"
+  config.username = "paypal.gmail.com"
+  config.password = "paypal-password"
+  config.signature = "paypal-signature"
 end
